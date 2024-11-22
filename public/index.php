b823b05c437c4d86ca1b14a4a0b94018b07876c9
Afonso Caluege <acaluege@outlook.com>
2024-11-22 17:23:33 +0100

    #02 Routing sytem

--- public/index.php
+++ public/index.php
@@ -1,2 +1,17 @@
 <?php
 
+require "../private/core/init.php";
+
+$controller = $_GET['page_name'] ?? "home";
+$controller = strtoLower($controller);
+
+if(file_exists("../app/controllers/".$controller . ".php"))
+{
+    require "../app/controllers/".$controller . ".php";
+}else{
+    echo "controller not found";
+}
+    
+
+
+debug($controller);
